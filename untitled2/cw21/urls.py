9e988501d56527e6be9943b190c49eb75e2914c1
aliaksandraburko <57365993+aliaksandraburko@users.noreply.github.com>
2020-03-03 16:45:43 +0200

    Delete urls.py

--- untitled2/cw21/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from cw21.views import home,add_customer
-
-urlpatterns=[
-    path('home', home, name='home'),
-    path('add', add_customer, name='add_customer'),
-
-]

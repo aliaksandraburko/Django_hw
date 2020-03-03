7646285aa6a09f6e69430a9fc4abf1f60c9c75dd
aliaksandraburko <57365993+aliaksandraburko@users.noreply.github.com>
2020-03-03 16:54:04 +0200

    Delete models.py

--- untitled2/myapp1/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class Customer(models.Model):
-    firstname = models.CharField(max_length=255, default=None)
-    lastname = models.CharField(max_length=255, default=None)
-    age = models.IntegerField()
